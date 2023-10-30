192a8b603a669d2e226b27602fdc64ba7808466a
edilbertoespinosa <edilbertoespinosa@gmail.com>
2023-10-30 14:38:17 -0600

    Se sube el panel del ultimo usuario en BD

--- src/components/LastMovieInDb.js
+++ src/components/LastMovieInDb.js
@@ -1,23 +1,45 @@
-import React from 'react';
-import imagenFondo from '../assets/images/mandalorian.jpg';
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+import imagenFondo from "../assets/images/mandalorian.jpg";
 
-function LastMovieInDb(){
-    return(
-        <div className="col-lg-6 mb-4">
-            <div className="card shadow mb-4">
-                <div className="card-header py-3">
-                    <h5 className="m-0 font-weight-bold text-gray-800">Last movie in Data Base</h5>
-                </div>
-                <div className="card-body">
-                    <div className="text-center">
-                        <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src={imagenFondo} alt=" Star Wars - Mandalorian "/>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa citationem ratione aperiam voluptatum non corporis ratione aperiam voluptatum quae dolorem culpa ratione aperiam voluptatum?</p>
-                    <a className="btn btn-danger" target="_blank" rel="nofollow" href="/">View movie detail</a>
-                </div>
-            </div>
+function LastMovieInDb() {
+  let [user, setUser] = useState({});
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/users")
+      .then((response) => response.json())
+      .then((data) => {
+        const users = data.users;
+        setUser(users[users.length - 1]);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div className="col-lg-6 mb-4">
+      <div className="card shadow mb-4">
+        <div className="card-header py-3">
+          <h5 className="m-0 font-weight-bold text-gray-800">
+            Last user in Data Base
+          </h5>
         </div>
-    )
+        <div className="card-body">
+          <div className="text-center card bg-dark text-white shadow">
+            <p>id : {user.id}</p>
+          </div>
+          <div className="text-center card bg-dark text-white shadow">
+            <p>name : {user.name}</p>
+          </div>
+          <div className="text-center card bg-dark text-white shadow">
+            <p>mail : {user.mail}</p>
+          </div>
+          <p>id : {user.id}</p>
+          <p>name : {user.name}</p>
+          <p>mail : {user.mail}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LastMovieInDb;
